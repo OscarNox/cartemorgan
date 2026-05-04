a53bdbb4fb45f7d782fb5863123e203adf1ed23d
Jose Oscar Ü Jerez <jerezsantosjoseoscar@gmail.com>
2026-05-04 01:34:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8665016509-fd2dc",
+  "appId": "1:77834707070:web:3b54c85f45c952bc3cb82f",
+  "apiKey": "AIzaSyBxxpUF_-LMaWFnnRbN4w5-BXeItY1QL4w",
+  "authDomain": "studio-8665016509-fd2dc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "77834707070"
+};
